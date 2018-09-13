866497b77695710e086bcb6369f18100b4146dbc
yang <18271955705@163.com>
2018-09-13 08:32:52 +0800

    修改了一些分类样式

--- js/category.js
+++ js/category.js
@@ -1,3 +1,36 @@
-mui('.mui-scroll-wrapper').scroll({
-	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-});
+$(function(){
+	mui('.mui-scroll-wrapper').scroll({
+		deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+	});
+	//获取一级分类数据
+	$.ajax({
+		type:"get",
+		url:"",
+		data:,
+		success:function(response){
+			
+		}
+	});
+	//获取二级分类数据
+	$("#links").on("click","a",function(){
+		
+		
+		$.ajax({
+			type:"get",
+			url:"",
+			data,
+			success:function(response){
+			
+			}
+		});
+	});
+})
+
+
+
+
+
+
+
+
+
